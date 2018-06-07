c58a41bccef4e23dcf23a03750c571697eb7aaaa
sudoQ <vorga88@gmail.com>
2018-06-07 13:11:30 +0300

    add line chart

--- src/js/main.js
+++ src/js/main.js
@@ -55,11 +55,11 @@ bar.append("text")
 
 // pie chart
 
-var pieData = [{name: "Septale Defekte/vaskuläre Fehlverbindungen", percent: 49.5},
-              {name: "Ursprungsanomalie der großen Arterien", percent: 8.8},
-              {name: "Linksobstruktionen", percent: 16.5},
-             {name: "Rechtsobstruktionen", percent: 17.3},
-            {name: "sonstige", percent: 8.2}]
+var pieData = [{ name: "Septale Defekte/vaskuläre Fehlverbindungen", percent: 49.5 },
+{ name: "Ursprungsanomalie der großen Arterien", percent: 8.8 },
+{ name: "Linksobstruktionen", percent: 16.5 },
+{ name: "Rechtsobstruktionen", percent: 17.3 },
+{ name: "sonstige", percent: 8.2 }]
 
 var svg = d3.select(".pie-chart"),
     width = svg.attr("width"),
@@ -100,3 +100,72 @@ arc.append("text")
         return "translate(" + label.centroid(d) + ")";
     })
     .text(function (d, i) { return d.data.percent + "%"; });
+
+
+//line chart
+
+var lineData = [{date: "2004", number: 0.002},
+{date: "2005", number: 0.010},
+{date: "2006", number: 0.020},
+{date: "2007", number: 0.025},
+{date: "2008", number: 0.050},
+{date: "2009", number: 0.080},
+{date: "2010", number: 0.450},
+{date: "2011", number: 0.700},
+{date: "2012", number: 0.900},
+{date: "2013", number: 1.000},
+{date: "2014", number: 1.400},
+{date: "2015", number: 1.650},
+{date: "2016", number: 1.800},
+{date: "2017", number: 2.000},
+{date: "2018", number: 2.215}];
+
+var margin = { top: 30, left: 30};
+var lineChart = d3.select(".line-chart"),
+    width = lineChart.attr("width") - margin.left * 2,
+    height = lineChart.attr("height") - margin.top * 2;
+
+
+var parseTime = d3.timeParse("%Y");
+
+// set the ranges
+var x = d3.scaleTime().range([0, width]);
+var y = d3.scaleLinear().range([height, 0]);
+
+// define the line
+var valueline = d3.line()
+    .x(function (d) { return x(d.date); })
+    .y(function (d) { return y(d.number); });
+
+// append the svg obgect to the body of the page
+// appends a 'group' element to 'svg'
+var lineChartG = lineChart
+    .append("g")
+    .attr("transform",
+        "translate(" + margin.left + "," + margin.top + ")");
+
+
+    // format the data
+    lineData.forEach(function (d) {
+        d.date = parseTime(+d.date);
+        d.number = +d.number * 1000;
+    });
+
+    // Scale the range of the data
+    x.domain(d3.extent(lineData, function (d) { return d.date; }));
+    y.domain([0, d3.max(lineData, function (d) { return d.number; })]);
+
+    // Add the valueline path.
+    lineChartG.append("path")
+        .data([lineData])
+        .attr("class", "line")
+        .attr("d", valueline);
+
+    // Add the X Axis
+    lineChartG.append("g")
+        .attr("transform", "translate(0," + height + ")")
+        .call(d3.axisBottom(x));
+
+    // Add the Y Axis
+    lineChartG.append("g")
+        .call(d3.axisLeft(y));
